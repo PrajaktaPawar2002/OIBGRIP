fe8e65ae266c03a209f65305ab4c50c79f109655
Prajakta <pawar.prajakta1410@gmail.com>
2023-01-27 12:58:33 +0530

    Intial commit

---
+++ Task 3/Temparature converter webiste/script.js
@@ -0,0 +1,44 @@
+
+	let celsius = document.getElementById('celsius');
+	let fahrenheit = document.getElementById('fahrenheit');
+	// let kelvin = document.getElementById('kelvin');
+
+	celsius.oninput = function () {
+		let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+		fahrenheit.value = parseFloat(f.toFixed(2));
+	
+		let k = (parseFloat(celsius.value) + 273.15);
+		kelvin.value = parseFloat(k.toFixed(2));
+	}
+	fahrenheit.oninput = function () {
+		let c = ((parseFloat(
+			fahrenheit.value) - 32) * 5) / 9;
+		celsius.value = parseFloat(c.toFixed(2));
+	
+		let k = (parseFloat(
+			fahrenheit.value) - 32) * 5 / 9 + 273.15;
+		kelvin.value = parseFloat(k.toFixed(2));
+	}
+	// kelvin.oninput = function () {
+	// 	let f = (parseFloat(
+	// 		kelvin.value) - 273.15) * 9 / 5 + 32;
+	// 	fahrenheit.value = parseFloat(f.toFixed(2));
+	
+	// 	let c = (parseFloat(kelvin.value) - 273.15);
+	// 	celsius.value = parseFloat(c.toFixed(2));
+	// }
+
+
+// let celsius = document.getElementById("celsius");
+// let fahrenheit = document.getElementById("fahrenheit");
+
+// function celToFar(){
+//     let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
+//     fahrenheit.value = parseFloat(output.toFixed(2));
+// }
+
+// function farToCel(){
+//     let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+//     celsius.value = parseFloat( output.toFixed(2));
+//     console.log(output);
+// }
